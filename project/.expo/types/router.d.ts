2fc2521fe9b97b00a0442596017a1db747f860ef
Xalex217 <aarosales321@gmail.com>
2024-10-24 12:41:37 -0700

    working navbar

--- project/.expo/types/router.d.ts
+++ project/.expo/types/router.d.ts
@@ -6,7 +6,7 @@ export * from 'expo-router';
 declare module 'expo-router' {
   export namespace ExpoRouter {
     export interface __routes<T extends string = string> extends Record<string, unknown> {
-      StaticRoutes: `/` | `/(app)` | `/(app)/Home/page` | `/(app)/NavBar` | `/(app)/Profile/Profile` | `/(app)/Search/Search` | `/(app)/Stats/Stats` | `/(app)/Workout/Workout` | `/(auth)` | `/(auth)/page` | `/Home/page` | `/NavBar` | `/Profile/Profile` | `/Search/Search` | `/Stats/Stats` | `/Workout/Workout` | `/_sitemap` | `/page`;
+      StaticRoutes: `/` | `/(app)` | `/(app)/Home/page` | `/(app)/NavBar` | `/(app)/Profile/Profile` | `/(app)/Search/Search` | `/(app)/Stats/Stats` | `/(app)/Workout/Workout` | `/(app)\NavBar` | `/(app)\Profile\Profile` | `/(app)\Search\Search` | `/(app)\Stats\Stats` | `/(app)\Workout\Workout` | `/(auth)` | `/(auth)/page` | `/Home/page` | `/NavBar` | `/Profile/Profile` | `/Search/Search` | `/Stats/Stats` | `/Workout/Workout` | `/_sitemap` | `/page`;
       DynamicRoutes: never;
       DynamicRouteTemplate: never;
     }
