4e8cc5f36dc680fb7f4d48b7be516c2b8429349f
Tyler Canepa <tmcanepa@ucsb.edu>
2024-10-24 00:18:34 -0700

    Got Working CreateUser Page

--- project/.expo/types/router.d.ts
+++ project/.expo/types/router.d.ts
@@ -6,7 +6,7 @@ export * from 'expo-router';
 declare module 'expo-router' {
   export namespace ExpoRouter {
     export interface __routes<T extends string = string> extends Record<string, unknown> {
-      StaticRoutes: `/` | `/(app)` | `/(app)/Home/page` | `/(auth)` | `/(auth)/page` | `/Home/page` | `/_sitemap` | `/page`;
+      StaticRoutes: `/` | `/(app)` | `/(app)/Home/page` | `/(auth)` | `/(auth)/Register` | `/(auth)/page` | `/Home/page` | `/Register` | `/_sitemap` | `/page`;
       DynamicRoutes: never;
       DynamicRouteTemplate: never;
     }
