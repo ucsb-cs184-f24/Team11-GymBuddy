c358fcb980433e627f8d8462a7975dae42ee30f5
Tyler Canepa <tmcanepa@ucsb.edu>
2024-10-30 15:04:19 -0700

    added logout to profile page

--- project/app/(app)/Profile/Profile.tsx
+++ project/app/(app)/Profile/Profile.tsx
@@ -1,11 +1,31 @@
 // Home.tsx
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Button, Alert } from "react-native";
+import { auth } from "@/firebaseConfig";
+import { useRouter } from "expo-router";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+
+
 
 const Profile = () => {
+  const router = useRouter(); 
+
+  const logout = async() => {
+    try {
+      await auth.signOut()
+      await AsyncStorage.removeItem("@user");
+      router.replace("/(auth)/page");
+      Alert.alert('Logged Out');
+    } catch (error) {
+      Alert.alert('Error logging out');
+    }
+  };
+  
   return (
     <View style={styles.container}>
       <Text style={styles.text}>Profile</Text>
+      <Button title="Logout" onPress={logout} color="#4a90e2" />
     </View>
   );
 };
