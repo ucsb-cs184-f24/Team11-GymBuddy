c5a4f7f659e5e22b79b83600ea6b2234a0079de0
Cory Zhao <coryzhao80796@gmail.com>
2024-10-31 15:40:48 -0700

    Done

--- project/app/(app)/Profile/Profile.tsx
+++ project/app/(app)/Profile/Profile.tsx
@@ -1,4 +1,4 @@
-// Home.tsx
+// Profile.tsx
 import React, { useEffect, useState } from "react";
 import {
   View,
@@ -6,50 +6,59 @@ import {
   StyleSheet,
   SafeAreaView,
   TouchableOpacity,
+  useWindowDimensions,
 } from "react-native";
 import { BarChart, barDataItem } from "react-native-gifted-charts";
 import workoutData from "@/data/workoutData.json";
-import { parseDateTime, processWeeklyData } from "./helpers/dataProcessHelpers";
+import {
+  parseDateTime,
+  processChartData,
+  getRangeForPeriod,
+} from "./helpers/dataProcessHelpers";
 import SegmentedControl from "@react-native-segmented-control/segmented-control";
 import { SymbolView } from "expo-symbols";
-import Card from "@/components/Card";
-
-enum Period {
-  week = "week",
-  month = "month",
-  year = "year",
-}
+import {
+  Period,
+  ChartCategory,
+  WorkoutDayResults,
+} from "@/app/(app)/Profile/helpers/dataProcessHelpers";
 
 const Profile = () => {
+  const { width, height } = useWindowDimensions();
   const [chartData, setChartData] = useState<barDataItem[]>([]);
-  const [chartPeriod, setChartPeriod] = useState<Period>(Period.week);
+  const [chartPeriod, setChartPeriod] = useState<Period>(Period.Week);
   const [currentDate, setCurrentDate] = useState<Date>(new Date());
   const [currentEndDate, setCurrentEndDate] = useState<Date>(new Date());
   const [chartKey, setChartKey] = useState<number>(0);
-  const [chartCategory, setChartCategory] = useState<"Workouts" | "Body">(
-    "Workouts"
+  const [chartCategory, setChartCategory] = useState<ChartCategory>(
+    ChartCategory.Workouts
   );
+
+  const periodOptions = Object.values(Period);
+  const chartCategoryOptions = Object.values(ChartCategory);
 
   useEffect(() => {
     const fetchData = async () => {
-      if (chartPeriod === Period.week) {
-        const { startDate, endDate } = getWeekRange(currentDate);
-        setCurrentEndDate(new Date(startDate));
+      const { startDate, endDate } = getRangeForPeriod(
+        chartPeriod,
+        currentDate
+      );
+      const data = filterData(startDate, endDate);
+      const processedData = processChartData(
+        data,
+        chartCategory,
+        chartPeriod,
+        currentDate
+      );
 
-        const data = fetchWeeklyData(startDate, endDate, chartCategory);
-        const processedData = processWeeklyData(data, chartCategory);
-        setChartData(processedData);
-        setChartKey((prev) => prev + 1);
-      }
+      setCurrentEndDate(new Date(startDate));
+      setChartData(processedData);
+      setChartKey((prev) => prev + 1);
     };
     fetchData();
-  }, [currentDate, chartCategory]);
+  }, [currentDate, chartCategory, chartPeriod]);
 
-  const fetchWeeklyData = (
-    startDate: number,
-    endDate: number,
-    category: "Workouts" | "Body"
-  ) => {
+  const filterData = (startDate: number, endDate: number) => {
     const filteredData = workoutData.filter((workout) => {
       const { year, zeroIndexedMonth, day, hour, minute } = parseDateTime(
         workout.date,
@@ -65,74 +74,122 @@ const Profile = () => {
       return workoutDate >= startDate && workoutDate <= endDate;
     });
 
-    if (category === "Workouts") {
-      // Group workouts by day of the week
-      const dayOfWeekCount: { [key: number]: number } = {};
+    const result: WorkoutDayResults = {};
 
-      filteredData.forEach((workout) => {
-        const { year, zeroIndexedMonth, day, hour, minute } = parseDateTime(
-          workout.date,
-          workout.time
-        );
-        const dayOfWeek = new Date(
-          year,
-          zeroIndexedMonth,
-          day,
-          hour,
-          minute
-        ).getDay();
-        dayOfWeekCount[dayOfWeek] = (dayOfWeekCount[dayOfWeek] || 0) + 1;
-      });
+    filteredData.forEach((workout) => {
+      const { year, zeroIndexedMonth, day, hour, minute } = parseDateTime(
+        workout.date,
+        workout.time
+      );
+      const workoutDate = new Date(
+        year,
+        zeroIndexedMonth,
+        day,
+        hour,
+        minute
+      ).toLocaleDateString();
 
-      return Object.entries(dayOfWeekCount).map(([day, total]) => ({
-        day_of_week: parseInt(day),
-        total,
-      }));
-    } else if (category === "Body") {
-      // Group workouts by body area
-      const bodyAreaCount: { [key: string]: number } = {};
+      // Initialize the date key if it doesn't exist
+      if (!result[workoutDate]) {
+        result[workoutDate] = {
+          totalWorkout: 0,
+          bodyAreas: {},
+        };
+      }
 
-      filteredData.forEach((workout) => {
-        const bodyArea = workout.bodyArea;
-        bodyAreaCount[bodyArea] = (bodyAreaCount[bodyArea] || 0) + 1;
-      });
+      // Increment totalWorkout for the date
+      result[workoutDate].totalWorkout += 1;
 
-      return Object.entries(bodyAreaCount).map(([body_area, total]) => ({
-        body_area,
-        total,
-      }));
+      const bodyArea = workout.bodyArea;
+      // Initialize the body area count if it doesn't exist
+      if (!result[workoutDate].bodyAreas[bodyArea]) {
+        result[workoutDate].bodyAreas[bodyArea] = 0;
+      }
+      result[workoutDate].bodyAreas[bodyArea] += 1;
+    });
+
+    return result;
+  };
+
+  const handlePreviousPeriod = () => {
+    if (chartPeriod === Period.Week) {
+      setCurrentDate(new Date(currentDate.setDate(currentDate.getDate() - 7)));
+    } else if (chartPeriod === Period.Month) {
+      // Sets currentDate to the end of the previous month
+      setCurrentDate(new Date(currentDate.setDate(0)));
+    } else if (chartPeriod === Period.Year) {
+      setCurrentDate(
+        new Date(currentDate.setFullYear(currentDate.getFullYear() - 1))
+      );
     }
-    return [];
   };
 
-  const getWeekRange = (date: Date) => {
-    const startOfWeek = new Date(date);
-    startOfWeek.setDate(startOfWeek.getDate() - 6);
-    startOfWeek.setHours(0, 0, 0, 0);
+  const handleNextPeriod = () => {
+    if (chartPeriod === Period.Week) {
+      // Prevents showing data in the future
+      if (
+        currentDate.getDate() === new Date().getDate() &&
+        currentDate.getFullYear() === new Date().getFullYear()
+      ) {
+        return;
+      }
 
-    const endOfWeek = new Date(date);
-    endOfWeek.setHours(23, 59, 59, 999);
+      setCurrentDate(new Date(currentDate.setDate(currentDate.getDate() + 7)));
+    } else if (chartPeriod === Period.Month) {
+      // Prevents showing data in the future
+      if (
+        currentDate.getMonth() === new Date().getMonth() &&
+        currentDate.getFullYear() === new Date().getFullYear()
+      ) {
+        return;
+      }
 
-    return {
-      startDate: startOfWeek.getTime(),
-      endDate: endOfWeek.getTime(),
-    };
-  };
+      // We want "monthly" to show a time range of the start and end of the month
+      // Sets tempDate to the final day of the next month (note - different months have different amount of days)
+      const tempDate = new Date(currentDate);
+      tempDate.setDate(1);
+      tempDate.setMonth(tempDate.getMonth() + 2);
+      tempDate.setDate(0);
 
-  const handlePreviousWeek = () => {
-    setCurrentDate(new Date(currentDate.setDate(currentDate.getDate() - 7)));
-  };
-
-  const handleNextWeek = () => {
-    setCurrentDate(new Date(currentDate.setDate(currentDate.getDate() + 7)));
+      // If at the "present" time frame, show a time range of start of month to today
+      if (
+        tempDate.getMonth() === new Date().getMonth() &&
+        tempDate.getFullYear() === new Date().getFullYear()
+      ) {
+        setCurrentDate(new Date());
+      } else {
+        setCurrentDate(tempDate);
+      }
+    } else if (chartPeriod === Period.Year) {
+      // Prevents showing data in the future
+      if (currentDate.getFullYear() === new Date().getFullYear()) {
+        return;
+      }
+      setCurrentDate(
+        new Date(currentDate.setFullYear(currentDate.getFullYear() + 1))
+      );
+    }
   };
 
   return (
-    <SafeAreaView style={{ margin: 15 }}>
-      <Card>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.analyticsContainer}>
+        <Text style={{ fontSize: 32, fontWeight: "700", marginBottom: 16 }}>
+          Analytics
+        </Text>
+        <SegmentedControl
+          values={periodOptions}
+          style={{ marginBottom: 10 }}
+          selectedIndex={periodOptions.indexOf(chartPeriod)}
+          onChange={(event) => {
+            const index = event.nativeEvent.selectedSegmentIndex;
+            setChartPeriod(periodOptions[index]);
+          }}
+        />
         <Text
           style={{
-            fontSize: 18,
+            fontSize: 14,
+            color: "#5A5A5A",
             fontWeight: "700",
             margin: 5,
             marginBottom: 20,
@@ -145,19 +202,21 @@ const Profile = () => {
         <BarChart
           key={chartKey}
           data={chartData}
+          parentWidth={width * 0.85}
+          adjustToWidth={true}
           height={200}
-          width={290}
-          barWidth={18}
           minHeight={3}
           barBorderRadius={3}
-          spacing={20}
           noOfSections={5}
           yAxisThickness={0}
           xAxisThickness={0}
-          xAxisLabelTextStyle={{ color: "gray" }}
+          xAxisLabelTextStyle={{
+            color: "gray",
+            fontSize: chartData.length > 25 ? 5 : 12,
+          }}
           yAxisTextStyle={{ color: "gray" }}
           animationDuration={300}
-          dashGap={10}
+          showFractionalValues={false}
           isAnimated
           showGradient
         />
@@ -171,7 +230,7 @@ const Profile = () => {
         >
           <TouchableOpacity
             style={{ alignItems: "center" }}
-            onPress={() => handlePreviousWeek()}
+            onPress={handlePreviousPeriod}
           >
             <SymbolView
               name="chevron.left.circle.fill"
@@ -182,17 +241,17 @@ const Profile = () => {
             <Text style={{ fontSize: 11, color: "gray" }}>prev</Text>
           </TouchableOpacity>
           <SegmentedControl
-            values={["Workouts", "Body"]}
+            values={chartCategoryOptions}
             style={{ width: 180 }}
-            selectedIndex={chartCategory === "Workouts" ? 0 : 1}
+            selectedIndex={chartCategoryOptions.indexOf(chartCategory)}
             onChange={(event) => {
               const index = event.nativeEvent.selectedSegmentIndex;
-              setChartCategory(index === 0 ? "Workouts" : "Body");
+              setChartCategory(chartCategoryOptions[index]);
             }}
           />
           <TouchableOpacity
             style={{ alignItems: "center" }}
-            onPress={() => handleNextWeek()}
+            onPress={handleNextPeriod}
           >
             <SymbolView
               name="chevron.right.circle.fill"
@@ -203,7 +262,7 @@ const Profile = () => {
             <Text style={{ fontSize: 11, color: "gray" }}>next</Text>
           </TouchableOpacity>
         </View>
-      </Card>
+      </View>
     </SafeAreaView>
   );
 };
@@ -211,7 +270,13 @@ const Profile = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#ECECEC",
+  },
+  analyticsContainer: {
+    width: "100%",
+    backgroundColor: "white",
+    padding: 15,
   },
 });
 
--- project/app/(app)/Profile/helpers/dataProcessHelpers.ts
+++ project/app/(app)/Profile/helpers/dataProcessHelpers.ts
@@ -1,73 +1,243 @@
-type WorkoutData = { day_of_week: number; total: number };
-type BodyData = { body_area: string; total: number };
+export enum Period {
+  Week = "Week",
+  Month = "Month",
+  Year = "Year",
+}
 
-type DataInput = WorkoutData | BodyData;
+export enum ChartCategory {
+  Workouts = "Workouts",
+  Body = "Body",
+}
 
-export const processWeeklyData = (
-  data: DataInput[],
-  category: "Workouts" | "Body"
+interface BodyAreas {
+  [bodyArea: string]: number;
+}
+
+interface WorkoutSummary {
+  totalWorkout: number;
+  bodyAreas: BodyAreas;
+}
+
+export interface WorkoutDayResults {
+  [date: string]: WorkoutSummary;
+}
+
+export const getRangeForPeriod = (period: Period, date: Date) => {
+  const startDate = new Date(date);
+  startDate.setHours(0, 0, 0, 0);
+  const endDate = new Date(date);
+  endDate.setHours(23, 59, 59, 999);
+
+  if (period === Period.Week) {
+    // Sets start date to 6 days ago (to cover last 7 days)
+    // Example: (Thu - Wed)
+    startDate.setDate(startDate.getDate() - 6);
+  } else if (period === Period.Month) {
+    // Sets start date to beginning of month
+    // Example: (Oct 1 - Oct 31), (Oct 1 - Oct 20)
+    startDate.setDate(1);
+  } else if (period === Period.Year) {
+    // Sets start date to 11 months ago (to cover last 12 months)
+    startDate.setMonth(startDate.getMonth() - 11);
+    // Sets start date to beginning of month to cover whole month
+    startDate.setDate(1);
+
+    // Sets end date to end of month to cover whole month
+    // Example (Dec 1, 2023 - Jan 31, 2024)
+    endDate.setMonth(endDate.getMonth() + 1);
+    endDate.setDate(0);
+  }
+
+  return {
+    startDate: startDate.getTime(),
+    endDate: endDate.getTime(),
+  };
+};
+
+const processBodyData = (data: WorkoutDayResults) => {
+  const labels = ["Chest", "Shoulders", "Arms", "Back", "Core", "Legs"];
+
+  let barData = labels.map((label) => ({
+    label,
+    value: 0,
+    frontColor: "#d1d5db",
+    gradientColor: "#d1d5db",
+  }));
+
+  Object.entries(data).forEach(([_, summary]) => {
+    Object.entries(summary.bodyAreas).forEach(([bodyArea, count]) => {
+      const indexOfBodyArea = barData.findIndex(
+        (bar) => bar.label === bodyArea
+      );
+      barData[indexOfBodyArea].value += count;
+    });
+  });
+
+  barData = barData.map((item) => ({
+    ...item,
+    frontColor: item.value >= 1 ? "#ffab00" : "#d1d5db",
+    gradientColor: item.value >= 1 ? "#ff0000" : "#d1d5db",
+  }));
+
+  return barData;
+};
+
+const processWeeklyWorkoutData = (
+  data: WorkoutDayResults,
+  currentDate: Date
 ) => {
-  if (category === "Workouts") {
-    const days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  let labels: string[] = [];
+  const { startDate, endDate } = getRangeForPeriod(Period.Week, currentDate);
+  const tempStartDate = new Date(startDate);
+  const tempEndDate = new Date(endDate);
 
-    let barData = days.map(
-      (label) =>
-        ({
-          label,
-          value: 0,
-          frontColor: "#d1d5db",
-          gradientColor: "#d1d5db",
-        } as any)
+  while (tempStartDate <= tempEndDate) {
+    labels.push(tempStartDate.toLocaleDateString());
+    tempStartDate.setDate(tempStartDate.getDate() + 1);
+  }
+
+  let barData = labels.map((label) => ({
+    label,
+    value: 0,
+    frontColor: "#d1d5db",
+    gradientColor: "#d1d5db",
+  }));
+
+  Object.entries(data).forEach(([date, summary]) => {
+    const indexOfDate = barData.findIndex((bar) => bar.label === date);
+    const isActive = summary.totalWorkout >= 1;
+    barData[indexOfDate].value = summary.totalWorkout;
+    barData[indexOfDate].frontColor = isActive ? "#ffab00" : "#d1d5db";
+    barData[indexOfDate].gradientColor = isActive ? "#ff0000" : "#d1d5db";
+  });
+
+  barData = barData.map((item) => {
+    const { year, zeroIndexedMonth, day } = parseDateString(item.label);
+    const dayString = new Date(year, zeroIndexedMonth, day).toLocaleDateString(
+      "en-US",
+      { weekday: "short" }
     );
+    return {
+      ...item,
+      label: dayString,
+    };
+  });
 
-    (data as WorkoutData[]).forEach((item) => {
-      const dayIndex = item.day_of_week;
-      if (dayIndex >= 0 && dayIndex < 7) {
-        barData[dayIndex].value = item.total;
+  return barData;
+};
 
-        if (item.total < 1) {
-          barData[dayIndex].frontColor = "#d1d5db";
-          barData[dayIndex].gradientColor = "#d1d5db";
-        } else {
-          barData[dayIndex].frontColor = "#ffab00";
-          barData[dayIndex].gradientColor = "#ff0000";
-        }
-      }
-    });
+const processMonthlyWorkoutData = (
+  data: WorkoutDayResults,
+  currentDate: Date
+) => {
+  let labels: string[] = [];
+  const { startDate, endDate } = getRangeForPeriod(Period.Month, currentDate);
+  const tempStartDate = new Date(startDate);
+  const tempEndDate = new Date(endDate);
 
-    const currentDayIndex = new Date().getDay();
-    barData = [
-      ...barData.slice(currentDayIndex + 1),
-      ...barData.slice(0, currentDayIndex + 1),
-    ];
+  while (tempStartDate <= tempEndDate) {
+    labels.push(tempStartDate.toLocaleDateString());
+    tempStartDate.setDate(tempStartDate.getDate() + 1);
+  }
 
-    return barData;
-  } else if (category === "Body") {
-    const bodyAreas = ["Chest", "Shoulders", "Arms", "Back", "Core", "Legs"];
+  let barData = labels.map((label) => ({
+    label,
+    value: 0,
+    frontColor: "#d1d5db",
+    gradientColor: "#d1d5db",
+  }));
 
-    let barData = bodyAreas.map((label) => ({
-      label,
-      value: 0,
-      frontColor: "#d1d5db",
-      gradientColor: "#d1d5db",
-    }));
+  Object.entries(data).forEach(([date, summary]) => {
+    const indexOfDate = barData.findIndex((bar) => bar.label === date);
+    const isActive = summary.totalWorkout >= 1;
+    barData[indexOfDate].value = summary.totalWorkout;
+    barData[indexOfDate].frontColor = isActive ? "#ffab00" : "#d1d5db";
+    barData[indexOfDate].gradientColor = isActive ? "#ff0000" : "#d1d5db";
+  });
 
-    (data as BodyData[]).forEach((item) => {
-      const bodyAreaIndex = bodyAreas.indexOf(item.body_area);
-      if (bodyAreaIndex >= 0) {
-        barData[bodyAreaIndex].value = item.total;
+  barData = barData.map((item) => {
+    const { day } = parseDateString(item.label);
+    return {
+      ...item,
+      label: day.toString(),
+    };
+  });
 
-        if (item.total < 1) {
-          barData[bodyAreaIndex].frontColor = "#d1d5db";
-          barData[bodyAreaIndex].gradientColor = "#d1d5db";
-        } else {
-          barData[bodyAreaIndex].frontColor = "#ffab00";
-          barData[bodyAreaIndex].gradientColor = "#ff0000";
-        }
-      }
-    });
+  return barData;
+};
 
-    return barData;
+const processYearlyWorkoutData = (
+  data: WorkoutDayResults,
+  currentDate: Date
+) => {
+  let labels: string[] = [];
+  const { startDate, endDate } = getRangeForPeriod(Period.Year, currentDate);
+  const tempStartDate = new Date(startDate);
+  const tempEndDate = new Date(endDate);
+
+  while (tempStartDate <= tempEndDate) {
+    labels.push(tempStartDate.getMonth().toString());
+    tempStartDate.setMonth(tempStartDate.getMonth() + 1);
+  }
+
+  let barData = labels.map((label) => ({
+    label,
+    value: 0,
+    frontColor: "#d1d5db",
+    gradientColor: "#d1d5db",
+  }));
+
+  Object.entries(data).forEach(([date, summary]) => {
+    const { zeroIndexedMonth } = parseDateString(date);
+    const indexOfMonth = barData.findIndex(
+      (bar) => bar.label === zeroIndexedMonth.toString()
+    );
+    const isActive = summary.totalWorkout >= 1;
+    barData[indexOfMonth].value += summary.totalWorkout;
+    barData[indexOfMonth].frontColor = isActive ? "#ffab00" : "#d1d5db";
+    barData[indexOfMonth].gradientColor = isActive ? "#ff0000" : "#d1d5db";
+  });
+
+  const months = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
+  barData = barData.map((item) => ({
+    ...item,
+    label: months[parseInt(item.label)],
+    frontColor: item.value >= 1 ? "#ffab00" : "#d1d5db",
+    gradientColor: item.value >= 1 ? "#ff0000" : "#d1d5db",
+  }));
+
+  return barData;
+};
+
+export const processChartData = (
+  data: WorkoutDayResults,
+  category: ChartCategory,
+  period: Period,
+  currentDate: Date
+) => {
+  if (category === ChartCategory.Body) {
+    return processBodyData(data);
+  } else if (category === ChartCategory.Workouts) {
+    if (period === Period.Week) {
+      return processWeeklyWorkoutData(data, currentDate);
+    } else if (period === Period.Month) {
+      return processMonthlyWorkoutData(data, currentDate);
+    } else if (period === Period.Year) {
+      return processYearlyWorkoutData(data, currentDate);
+    }
   }
   return [];
 };
@@ -78,4 +248,11 @@ export const parseDateTime = (date: string, time: string) => {
 
   const zeroIndexedMonth = month - 1;
   return { year, zeroIndexedMonth, day, hour, minute };
+};
+
+export const parseDateString = (date: string) => {
+  const [month, day, year] = date.split("/").map(Number);
+
+  const zeroIndexedMonth = month - 1;
+  return { year, zeroIndexedMonth, day };
 };
