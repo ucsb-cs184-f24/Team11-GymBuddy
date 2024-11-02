0f1c2f1017c84b5d3b1b86f04f37d43f15f63de0
liamlizard <82777456+liamlizard@users.noreply.github.com>
2024-11-01 22:39:10 -0700

    Fixed routes

--- project/.expo/types/router.d.ts
+++ project/.expo/types/router.d.ts
@@ -6,7 +6,7 @@ export * from 'expo-router';
 declare module 'expo-router' {
   export namespace ExpoRouter {
     export interface __routes<T extends string = string> extends Record<string, unknown> {
-      StaticRoutes: `/` | `/(app)/(auth)/Register` | `/(app)/Register` | `/(auth)` | `/(auth)/Register` | `/(auth)/SignIn` | `/(tabs)` | `/(tabs)/Home` | `/(tabs)/Profile` | `/(tabs)/Search` | `/(tabs)/Stats` | `/(tabs)/Workout` | `/Home` | `/Profile` | `/Register` | `/Search` | `/SignIn` | `/Stats` | `/Workout` | `/_sitemap` | `/components/ProfileData` | `/components/pickImage`;
+      StaticRoutes: `/` | `/(auth)` | `/(auth)/Register` | `/(auth)/SignIn` | `/(tabs)` | `/(tabs)/Home` | `/(tabs)/Profile` | `/(tabs)/Search` | `/(tabs)/Stats` | `/(tabs)/Workout` | `/Home` | `/Profile` | `/Register` | `/Search` | `/SignIn` | `/Stats` | `/Workout` | `/_sitemap` | `/components/ProfileData` | `/components/pickImage`;
       DynamicRoutes: never;
       DynamicRouteTemplate: never;
     }
