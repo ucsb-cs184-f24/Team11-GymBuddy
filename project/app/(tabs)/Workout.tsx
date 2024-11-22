2da77427ba04d7181ed2330f9a8566a65c30bb1f
ChrisWoolson <cw.woolson@gmail.com>
2024-11-22 10:35:25 -0800

    Merge Branch

--- project/app/(tabs)/Workout.tsx
+++ project/app/(tabs)/Workout.tsx
@@ -12,9 +12,16 @@ import {
   TextInput,
   Animated,
 } from 'react-native';
-import { BlurView } from 'expo-blur';
-import { getUserId, createPost, getWorkouts, WorkoutLog } from '@/serviceFiles/databaseService';
-import { workoutsByCategory } from '@/utils/Workout/workoutCatagory';
+import { SafeAreaView } from "react-native-safe-area-context";
+import { LinearGradient } from "expo-linear-gradient";
+import { BlurView } from "expo-blur";
+import {
+  createPost,
+  getWorkouts,
+  WorkoutLog,
+} from "@/serviceFiles/databaseService";
+import { getUserId } from "@/serviceFiles/usersDatabaseService";
+import { workoutsByCategory } from "@/utils/Workout/workoutCatagory";
 
 const { width } = Dimensions.get("window");
 
