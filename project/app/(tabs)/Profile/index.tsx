848cb8171b9a50d4488b6cd8100e69f9ac99a47e
liamlizard <82777456+liamlizard@users.noreply.github.com>
2024-11-25 15:15:44 -0800

    export UserData

--- project/app/(tabs)/Profile/index.tsx
+++ project/app/(tabs)/Profile/index.tsx
@@ -11,47 +11,13 @@ import {
 } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { useRouter } from "expo-router";
-import { getUserProfile, getUserId } from "@/serviceFiles/usersDatabaseService";
+import { getUserProfile, getUserId, UserData } from "@/serviceFiles/usersDatabaseService";
 import { MaterialCommunityIcons } from '@expo/vector-icons'; 
-import { getWorkouts } from "@/serviceFiles/postsDatabaseService";
+import { getWorkouts, WorkoutLog } from "@/serviceFiles/postsDatabaseService";
 import { auth } from "@/serviceFiles/authService";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const { width } = Dimensions.get("window");
-
-interface UserData {
-  bio: string;
-  createdAt: number;
-  email: string;
-  firstName: string;
-  followerCount: number;
-  followingCount: number;
-  gender: string;
-  height: number | null;
-  isPrivate: boolean;
-  lastName: string;
-  profilePicture: string;
-  username: string;
-  weight: number | null; 
-}
-
-export interface WorkoutLog {
-  caption: string;
-  commentsCount: number;
-  createdAt: number;
-  image: string;
-  likesCount: number;
-  exercises: {
-    name: string;
-    sets: number;
-    reps: number;
-    weight: number;
-    category: string;
-  }[]
-  userId: string;
-  username?: string;
-
-}
 
 export default function Profile() {
   const router = useRouter();
